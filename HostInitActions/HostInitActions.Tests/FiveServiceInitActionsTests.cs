50a975ad425191a9aca7bdf01efc72f4bbc94661
oscadall <oscadall@gmail.com>
2022-11-22 14:31:51 +0100

    celanup

--- HostInitActions/HostInitActions.Tests/FiveServiceInitActionsTests.cs
+++ HostInitActions/HostInitActions.Tests/FiveServiceInitActionsTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using HostInitActions.Tests.TestServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
